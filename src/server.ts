c8b15b15ca30a584c8e8331f454b4b6470c2d02f
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-15 20:15:58 -0300

    chore: update dependencies and improve server code
    - Added @biomejs/biome and ultracite to devDependencies in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,11 @@ app.get('/hello', () => {
   return 'hello fastify'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    // biome-ignore lint/suspicious/noConsole: ignore
+    console.log('HTTP Server Running!')
+  })
