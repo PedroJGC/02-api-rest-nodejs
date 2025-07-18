a216a9bc9bf96ff048320159ad0da52c91bb5341
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-17 21:51:36 -0300

    feat: performing queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
+import crypto from 'node:crypto'
 import fastify from 'fastify'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions').select('*')
 
-  return tables
+  return transactions
 })
 
 app
